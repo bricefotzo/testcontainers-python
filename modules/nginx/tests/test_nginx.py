7782598812410b7fa8efd130c7fa00f54fca6aff
bricefotzo <brice.fotzo@hotmail.com>
2024-03-11 01:24:11 +0100

    chore: remove unnecessary tests

--- modules/nginx/tests/test_nginx.py
+++ modules/nginx/tests/test_nginx.py
@@ -10,12 +10,3 @@ def test_docker_run_nginx():
         r = requests.get(url)
         assert r.status_code == 200
         assert "Welcome to nginx!" in r.text
-
-
-def test_docker_run_with_dockerfile():
-    nginx_container = NginxContainer("my-local-nginx", dockerfile="modules/nginx/tests/assets", port=8080)
-    with nginx_container as nginx:
-        url = f"http://{nginx.get_container_host_ip()}:{nginx.get_exposed_port(nginx.port)}/"
-        r = requests.get(url)
-        assert r.status_code == 200
-        assert "Custom Title" in r.text
